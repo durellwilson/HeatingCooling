de15fa5cbdcb1725cd5a2723113955b778a5621a
durellwilsondetroit@gmail.com <durellwilsondetroit@gmail.com>
2024-08-22 09:15:47 -0400

    another commit

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ if (actualTemp > desiredTemp) {
 } else {
     console.log("Standby");
 }
+
+let tempCelsius = 25;
+let targetUnit = "F";
+
+switch (targetUnit) {
+    case "C":
+        console.log(tempCelsius);
+        break;
+    case "F":
+        console.log((tempCelsius * 9/5) + 32);
+        break;
+    case "K":
+        console.log(tempCelsius + 273.15);
+        break;
+    default:
+        console.log("Invalid unit specified.");
+}
