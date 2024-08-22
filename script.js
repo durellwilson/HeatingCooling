98f2031cd14892da9d9a1294fd7ad27442808bd6
durellwilsondetroit@gmail.com <durellwilsondetroit@gmail.com>
2024-08-22 09:13:43 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let actualTemp = 75;
+let desiredTemp = 72;
+
+if (actualTemp > desiredTemp) {
+    console.log("Run A/C");
+} else if (actualTemp < desiredTemp) {
+    console.log("Run heat");
+} else {
+    console.log("Standby");
+}
